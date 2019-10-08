bd12675d1bbd1450880ceaf9230d58c4fc5ada45
Ricardo <568082704@qq.com>
2019-10-08 22:33:01 +0800

    save

--- test.jsx
+++ test.jsx
@@ -1,34 +1,63 @@
-function App(props) {
-  return(
-    <div>
-      <div className="header">
-        <Time1 />
-        <Judge />
-        <Time2 />
-      </div>
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      result1:0,
+      result2:0
+    }
+    this.t0 = new Date()
+  }
+  success1(x){
+    console.log(x);
+    console.log('兔子跑完了,用时')
+    let r1 = new Date()-this.t0
+    this.setState({
+      result1:r1
+    })
+  }
+  success2(x){
+    console.log(x);
+    console.log('乌龟跑完了,用时')
+    let r2 = new Date()-this.t0
+    this.setState({
+      result2:r2
+    })
+  }
+  
+  render(){ 
+    return(
       <div>
-        <Track1 n={0.1}/>
-        <Track2 />
+        <div className="header">
+          <Time1 result={this.state.result1}/>
+          <Judge/>
+          <Time2 result={this.state.result2} />
+        </div>
+        <div>
+          <Playground success1={this.success1.bind(this)}
+          success2 = {this.success2.bind(this)}/>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 
 
-function Time1() {
+function Time1(props) {
   return (
     <div>
       <h2>兔子用时</h2>
-      <div>0</div>
+      <div>{props.result}</div>
     </div>
   )
 }
-function Time2() {
+function Time2(props) {
   return (
     <div>
       <h2>乌龟用时</h2>
-      <div>0</div>
+      <div>{props.result}</div>
     </div>
   )
 }
@@ -54,14 +83,18 @@ class Track1 extends React.Component{
     let n = 0
     this.timerID=setInterval(
       () => {
-        n+=10
+        n+=25
         this.setState({
           styleT:{
             transform:`translateX(${n}%)`
           }
         })
+        if(n>=100){
+          clearInterval(this.timerID)
+          this.props.success('我是小兔兔')
+        }
       }, 
-      500);
+      1000);
   }   
   render(){
     return(
@@ -86,12 +119,16 @@ class Track2 extends React.Component{
     let n = 0
     this.timerID=setInterval(
       () => {
-        n+=10
+        n+=20
         this.setState({
           styleT:{
             transform:`translateX(${n}%)`
           }
         })
+        if(n>=100){
+          clearInterval(this.timerID)
+          this.props.success('我是小龟龟')
+        }
       }, 
       1000);
   }   
@@ -105,7 +142,14 @@ class Track2 extends React.Component{
   }
 }
 
-
+function Playground() {
+  return(
+    <div>
+      <Track1 />
+      <Track2 />
+    </div>
+  )
+}
 
 
 
